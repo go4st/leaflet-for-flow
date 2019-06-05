3130e7b8e0b3167087f58ba140441d262d4a1757
August Gilg <august.gilg@hswt.de>
2019-06-05 15:33:31 +0200

    Add value change listener function

--- src/main/java/org/vaadin/leaflet/LeafletPointSelector.java
+++ src/main/java/org/vaadin/leaflet/LeafletPointSelector.java
@@ -4,12 +4,15 @@ import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.function.SerializableConsumer;
+import com.vaadin.flow.shared.Registration;
 import elemental.json.JsonArray;
 import elemental.json.JsonObject;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Tag("div")
 @JavaScript("//unpkg.com/leaflet@1.3.4/dist/leaflet.js")
@@ -20,11 +23,14 @@ public class LeafletPointSelector extends Component implements HasSize {
 
     private final String id = UUID.randomUUID().toString();
 
-    private Collection<LeafletPoint> points;
+    private Collection<LeafletPoint> points = new ArrayList<>();
+
+    private LeafletPoint activePoint;
+
+    private List<Consumer<LeafletPoint>> valueChangeListeners = new ArrayList<>();;
 
     public LeafletPointSelector() {
         setId(id);
-        points = new ArrayList<>();
     }
 
     @Override
@@ -68,14 +74,29 @@ public class LeafletPointSelector extends Component implements HasSize {
     private void update(JsonArray jsPoints) {
 
         points.clear();
+        activePoint = null;
 
         for (int i = 0; i < jsPoints.length(); i++) {
             JsonObject jsPoint = jsPoints.get(i);
             LeafletPoint point = new LeafletPoint(jsPoint.get("latitude").asNumber(), jsPoint.get("longitude").asNumber());
             point.setId((int) jsPoint.getNumber("id"));
-            point.setActiveMarker(jsPoint.getBoolean("active"));
+
+            boolean active = jsPoint.getBoolean("active");
+            point.setActiveMarker(active);
+            if (active) {
+                activePoint = point;
+            }
+
             points.add(point);
         }
 
+        if (activePoint != null) {
+            valueChangeListeners.forEach(consumer -> consumer.accept(activePoint));
+        }
+    }
+
+    public Registration addValueChangeListener(Consumer<LeafletPoint> callback) {
+        valueChangeListeners.add(callback);
+        return () -> valueChangeListeners.remove(callback);
     }
 }
